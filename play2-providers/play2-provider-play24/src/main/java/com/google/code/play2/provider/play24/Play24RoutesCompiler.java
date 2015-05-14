4b5eebab3dabeeca89f0e3887b4ca00dd8d988e3
Grzegorz Slowikowski <gslowikowski@gmail.com>
2015-05-14 13:25:06 +0000

    #71 - Implement all new router features available in Play! 2.4.x - additional "controllers.Assets.Asset" value in generated import statements (for both: java and scala).
    git-svn-id: http://play2-maven-plugin.googlecode.com/svn/trunk/plugin@1234 0a1f0b36-0e7f-8a7b-3831-50ad4cbb12b1

--- play2-providers/play2-provider-play24/src/main/java/com/google/code/play2/provider/play24/Play24RoutesCompiler.java
+++ play2-providers/play2-provider-play24/src/main/java/com/google/code/play2/provider/play24/Play24RoutesCompiler.java
@@ -36,9 +36,9 @@ import com.google.code.play2.provider.api.RoutesCompilationException;
 public class Play24RoutesCompiler
     implements Play2RoutesCompiler
 {
-    private static final String[] javaAdditionalImports = new String[] { "play.libs.F" };
+    private static final String[] javaAdditionalImports = new String[] { "controllers.Assets.Asset", "play.libs.F" };
 
-    private static final String[] scalaAdditionalImports = new String[] {};
+    private static final String[] scalaAdditionalImports = new String[] { "controllers.Assets.Asset" };
 
     private String mainLang;
 
